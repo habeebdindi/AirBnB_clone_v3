ab91012f9b08339d43dacaab9c570f90b73c4f42
ManNjoro <elijohnmwoho@gmail.com>
2023-10-02 18:14:45 -0400

    fixed delete

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -30,6 +30,8 @@ def delete_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
+    for city in state.cities:
+        storage.delete(city)
     storage.delete(state)
     storage.save()
     return jsonify({}), 200
