f45e036ff6e86bd7a59863eb674f2aaeb7bd29cd
ManNjoro <elijohnmwoho@gmail.com>
2023-10-02 16:12:10 -0400

    fixed errors on users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -8,7 +8,7 @@ from models import storage
 from models.user import User
 
 
-@app_views.route('/users/', methods=['GET'])
+@app_views.route('/users/', methods=['GET'], strict_slashes=False)
 def get_users():
     """returns all user objects"""
     users = storage.all(User).values()
@@ -39,7 +39,7 @@ def delete_user(user_id):
     return jsonify({}), 200
 
 
-@app_views.route('/users/', methods=['POST'])
+@app_views.route('/users/', methods=['POST'], strict_slashes=False)
 def create_user():
     """creates an user object"""
     data = request.get_json()
@@ -58,14 +58,14 @@ def create_user():
 @app_views.route('/users/<user_id>', methods=['PUT'])
 def update_user(user_id):
     """updates an user object matching the id """
-    user = storage.get(user, user_id)
+    user = storage.get(User, user_id)
     if user is None:
         abort(404)
     data = request.get_json()
     if data is None:
         return jsonify({"error": "Not a JSON"}), 400
     for key, value in data.items():
-        if key not in ['id', 'created_at', 'updated_at']:
+        if key not in ['id', 'email', 'created_at', 'updated_at']:
             setattr(user, key, value)
     storage.save()
     return jsonify(user.to_dict()), 200
